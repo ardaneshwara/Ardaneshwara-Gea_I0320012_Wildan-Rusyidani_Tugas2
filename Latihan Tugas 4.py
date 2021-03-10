a32c26323155e8cf9cbd7ccca3411401a08be413
ardaneshwara <80144925+ardaneshwara@users.noreply.github.com>
2021-03-10 05:54:00 -0800

    Delete Latihan Tugas 4.py

--- Latihan Tugas 4.py
+++
@@ -1,4 +0,0 @@
-print("===== Menghitung Konversi Suhu Celsius ke Farenheit =====")
-celcius = float(input("Masukkah suhu celsius"))
-konversi = ((9/5)*celcius)+32
-print("Konversi suhu celcius ke farenheit nya adalah", konversi, "derajat")
