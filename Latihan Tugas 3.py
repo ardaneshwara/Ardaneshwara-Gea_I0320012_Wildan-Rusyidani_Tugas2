ff6514ea960bbca3e1387c7b7426370dc9f2742b
ardaneshwara <80144925+ardaneshwara@users.noreply.github.com>
2021-03-10 05:53:48 -0800

    Delete Latihan Tugas 3.py

--- Latihan Tugas 3.py
+++
@@ -1,6 +0,0 @@
-print("===== Program Menghitung Luas Permukaan Kubus =====")
-
-sisi = float(input("Masukkan sisi"))
-luas_permukaan = 6*sisi*sisi
-print("Luas permukaan kubus adalah", luas_permukaan, "satuan")
-
