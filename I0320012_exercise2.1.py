725db62b5adb7fc17a80437c57461b72e3252290
Ardaneshwara Gea <ardaneshwaragea2@gmail.com>
2021-03-09 22:13:10 -0800

    Pengerjaan Tugas Exercise

---
+++ I0320012_exercise2.1.py
@@ -0,0 +1,19 @@
+#tipe data Boolean
+print(True)
+
+#tpe data String
+print("Ayo Belajar Phyton")
+print('Belajar Phyton Sangat Mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
+
