5a3d576251814b35de60050a9d427ffb9d0deea7
Ardaneshwara Gea <ardaneshwaragea2@gmail.com>
2021-03-11 03:22:30 -0800

    Pengerjaan Tugas Latihan

---
+++ I0320012_soal2_tugas2.py
@@ -0,0 +1,52 @@
+#Memasukkan data pribadi
+tahun_lahir = int((2000 * 2) - 1998)
+tanggal_lahir = int(5 * 5)
+usia_tahun = int(36/2)
+usia_bulan = int(12-1)
+usia_hari = int(((12*30) + 5) - ((31-11) + 25))
+agama = "kristen protestan"
+tahun_lulus = int((4000/2) + (10*2))
+N_I_M = "I0320012"
+kelas = "A"
+
+#Memasukkan data alamat
+R_T = int(16/4)
+R_W = int(2*2)
+nomor_rumah = 7
+kode_pos = 57151
+kelurahan = "Kemlayan"
+kecamatan = "Serengan"
+
+#Menampilkan data tambahan
+tinggi = int((100*2) - 44)
+ukuran_sepatu = int(6*6)
+berat_badan = float((8*5) + 6.5)
+ukuran_baju = "S"
+hobi = "fotografi"
+
+#Menampilkan data organisasi
+organisasi_sma = "OSIS"
+posisi_sma = "Bendahara Sekbid Ketuhanan"
+organisasi_kuliah = "MNB HMTI"
+posisi_kuliah = "Sekretaris"
+
+print("Haii teman teman, perkenalkan namaku Ardaneshwara Gea.")
+print("Teman-teman ku biasa memanggil namaku Gea, tetapi keluarga ku memanggilku Ines.")
+print("Aku lahir di Sukoharjo,", str(tanggal_lahir), "April", str(tahun_lahir)+".")
+print("Tahun ini aku berumur", usia_tahun, "tahun,", usia_bulan, "bulan", "dan sekitar", usia_hari, "hari.")
+print("Aku memeluk agama", str(agama)+".")
+print("Alamat aku tinggal adalah di Jalan Gambir Anom", "No", str(nomor_rumah)+",", "RT", str(R_T)+",", "RW", str(R_W)+",", str(kelurahan)+",", str(kecamatan)+",", "Surakarta, Jawa Tengah.")
+print("Kode pos di area tempat tinggal ku adalah", str(kode_pos)+".")
+print("Aku merupakan alumni dari SMA Negeri 1 Surakarta dan lulus di tahun", str(tahun_lulus)+".")
+print("Sekarang aku bersyukur karena dapat berkuliah di Universitas Sebelas Maret, di Fakultas Teknik Industri.")
+print("Alasan ku memilih teknik industri karena melihat prospek kerja yang luas, tidak hanya belajar ilmu teknik saja, dan juga karena keinginan orang tua.")
+print("Nomor Induk Mahasiswa ku adalah", str(N_I_M)+",", "dan aku ditempatkan di kelas", str(kelas)+".")
+print("Saat ini tinggi ku mencapai", tinggi, "cm.")
+print("Terakhir saat aku mengukur berat badan, berat badan ku adalah", berat_badan, "kg.")
+print("Ukuran sepatuku sekarang adalah", str(ukuran_sepatu)+",", "dan saat memilih baju ukuran ku adalah", str(ukuran_baju)+".")
+print("Aku suka semua yang terkait dengan", str(hobi)+",", "karena menurutku dengan fotografi semua kenangan bisa diingat selamanya.")
+print("Pengalaman organisasi yang pernah aku ikuti semasa SMA adalah menjadi", posisi_sma, "di organisasi", str(organisasi_sma)+".")
+print("Di kuliah ini aku dipercaya menjadi", posisi_kuliah, "di organisasi kemahasiswaan yaitu", str(organisasi_kuliah)+".")
+print("Sekian perkenalan dari aku, terima kasih banyak.")
+
+
